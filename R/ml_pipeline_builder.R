fa69cacfdefe954449db38e43d551490d09797b8
Alex Ioannides <alex.ioannides@yahoo.co.uk>
2016-12-09 13:21:56 +0000

    Update documentation and README.

--- R/ml_pipeline_builder.R
+++ R/ml_pipeline_builder.R
@@ -13,28 +13,76 @@
 # limitations under the License.
 
 
-#' Build Machine Learning Pipelines
+#' Build machine learning pipelines
+#'
+#' Building machine learning models often requires pre- and post-transformation of the input and/or
+#' response variables, prior to training (or fitting) the models. For example, a model may require
+#' training on the logarithm of the response and input variables. As a consequence, fitting and
+#' then generating predictions from these models requires repeated application of transformation and
+#' inverse-transormation functions, to go from the original input to original output variables (via
+#' the model).
+#'
+#' This function produces an object in which it is possible to: define transformation and
+#' inverse-transformation functions; fit a model on training data; and then generate a prediction
+#' (or model-scoring) function that automatically applies the entire pipeline of transformation and
+#' inverse-transformation to the inputs and outputs of the inner-model's predicted scores.
+#'
+#' Calling \code{ml_pipline_builder()} will return an 'ml_pipeline' object (actually an environment
+#' or closure), whose methods can be accessed as one would access any element of a list. For example,
+#' \code{ml_pipline_builder()$transform_features} will allow you to get or set the
+#' \code{transform_features} function to use the pipeline. The full list of methods are:
+#'
+#' \itemize{
+#' \item \code{transform_features} - a unary function of a data.frame that returns a new data.frame
+#' containing only the transformed input variables - an error will be thrown if this is not the
+#' case (unless the function has been left undefined).
+#'
+#' \item \code{transform_response} - a unary function of a data.frame that returns a new data.frame
+#' containing only the transformed response variables - an error will be thrown if this is not the
+#' case (unless the function has been left undefined).
+#'
+#' \code{inv_transform_response} - this is the inverse of the \code{transform_response} function, a
+#' unary function of a data.frame that takes raw model output and transforms it back into the space
+#' containing the original data, returning a data.frame containing only this variable. An error
+#' will be thrown if any of these criteria are not met (unless the function has been left
+#' undefined).
+#'
+#' \item \code{estimate_model} - a unary function of a data.frame that returns a fitted model
+#' object, which must have a \code{predict.{model-class}} defined and available in the enclosing
+#' environment. An error will be thrown if any of these criteria are not met.
+#'
+#' \item \code{pipeline_fit} - a unary function of the input data that will apply (if defined)
+#' \code{transform_features} and \code{transform_response}, before executing \code{estimate_model} to
+#' estimate the model and create the end-to-end model pipeline.
+#'
+#' \item \code{pipeline_predict} - returns the model prediction pipeline (otherwise NULL).
+#'
+#' \item \code{model_estimate} - returns the estimated inner model object used in the pipeline
+#' (otherwise NULL).
+#' }
 #'
 #' @export
 #'
 #' @return
 #'
 #' @examples
+#' \dontrun{
+#' }
 ml_pipline_builder <- function() {
-  #
+  # capture the contents of the local environment
   this <- environment()
 
-  #
+  # define fields for estimated models and pipeline prediction functions
   model_estimate <- NULL
   pipeline_predict <- NULL
 
-  #
+  # define methods for transformation and estimation that need to be set
   transform_features <- function(x) NULL
   transform_response <- function(x) NULL
   inv_transform_response <- function(x) NULL
   estimate_model <- function(x) NULL
 
-  #
+  # method for fitting the pipeline and which creates and sets the model pipeline
   pipeline_fit <- function(data) {
     check_data_frame_throw_error(data, "data")
 
@@ -49,7 +97,7 @@ ml_pipline_builder <- function() {
 
     model_estimate <<- estimate_model(model_data)
 
-    #
+    # assemble and set the pipeline prediction object
     pipeline_predict <<- function(data) {
       check_data_frame_throw_error(data, "data")
 
@@ -66,6 +114,7 @@ ml_pipline_builder <- function() {
     }
   }
 
+  # return the object
   structure(this, class = c("ml_pipeline"))
 }
 
--- README.md
+++ README.md
@@ -3,4 +3,10 @@
 Machine Learning Pipelines for R
 ================================
 
-As inspired by the machine learning pipelines used in Apache Spark and Sci-Kit learn.
+Building machine learning models often requires pre- and post-transformation of the input and/or response variables, prior to training (or fitting) the models. For example, a model may require training on the logarithm of the response and input variables. As a consequence, fitting and then generating predictions from these models requires repeated application of transformation and inverse-transormation functions, to go from the original input to original output variables (via the model).
+
+This package is inspired by the machine learning pipelines used in Apache Spark, and provides a common intfernace with which it is possible to:
+
+-   define transformation and inverse-transformation functions;
+-   fit a model on training data; and then,
+-   generate a prediction (or model-scoring) function that automatically applies the entire pipeline of transformation and inverse-transformation to the inputs and outputs of the inner-model's predicted scores.
